7513fd63b0bd10b8e599707612b34a773859f6e1
Xhuing0603 <63singj@gmail.com>
2025-03-29 15:49:48 +0800

    feat: Upload 【AtCoder】Beginner Contest 182 - A. twiblr

---
+++ AtCoder/Beginner Contest 182/A. twiblr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const int MAXN = 1e7 + 50;
+const int Mod = 1e9 + 7;
+
+int a, b;
+
+int main(){
+    opt
+
+    cin >> a >> b;
+    cout << 2 * a + 100 - b << "\n";
+}
