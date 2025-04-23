db82426d64a7b4049b0721ed6187046b92860ec8
Xhuing0603 <63singj@gmail.com>
2025-04-23 12:47:51 +0800

    feat: Upload 【Kattis】Kattis Jack-O'-Lantern Juxtaposition

---
+++ Kattis/Kattis Jack-O'-Lantern Juxtaposition.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const int MAXN = 1e7 + 50;
+const int Mod = 1e9 + 7;
+typedef long long ll;
+
+int main(){
+    opt
+
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << a * b * c << endl;
+}
